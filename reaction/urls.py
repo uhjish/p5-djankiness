4e13d39c5cbdfec89cab796dd8bc5c09406127b4
uhjish <ajish@rootedinsights.com>
2015-10-07 01:33:45 -0400

    basic rest api skeletoned out

--- reaction/urls.py
+++ reaction/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+from reaction.axxun import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/',include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
