41d6b733c6db16383c66f62a3283076bb5728073
uhjish <ajish@rootedinsights.com>
2015-10-08 02:13:45 -0400

    remove cruft

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -2,6 +2,6 @@ var React = require('react')
 
 module.exports = React.createClass({
    render: function(){
-       return <h1>Hello, world.</h1>
+     return <h1>These are not the droids you're looking for.</h1>
    }
 })
