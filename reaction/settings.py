cf74d6b85f781c1d55df0768dbc1fce303d58e88
uhjish <ajish@rootedinsights.com>
2015-10-09 23:50:54 -0400

    db settings for heroku

--- reaction/settings.py
+++ reaction/settings.py
@@ -122,3 +122,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+#this overrides the sqlite3 stuff with heroku db url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+
+
